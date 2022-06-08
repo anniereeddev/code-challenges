f59d8cd4d29bdeb1da4e92be3d89d69ab28124de
anniereeddev <anniereed.dev@gmail.com>
2022-06-08 12:57:49 -0600

    Add first attempt at 6-7 coding challenge.

--- CC 2022-6-7/app.js
+++ CC 2022-6-7/app.js
@@ -6,3 +6,36 @@
 
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+//P: If input is empty/null, return empty array. Want: the amount of positive numbers, sum of negative numbers. 0 doesn't count since it's not positive or negative. 
+//R: Array with: [Count of positive numbers, sum of negative numbers].
+//E: 
+//P: Make a function that takes in an array. 
+
+//first attempt
+function countPositiveSumNegative(arr){
+    let result = []
+    let count = 0
+    let sum = 0
+    //Determine whether numbers in array are positive or negative.
+    arr.map(e=>
+        {if(e>0){
+            count += 1
+        }else if(e<0){
+            sum += e
+        }}
+        ) 
+
+    return arr
+    //If it's positive, count how many positive numbers there are.
+    //If it's negative, sum the negative numbers together. 
+    //If it's empty or null, return an empty array. 
+    //Put results into an array
+    //Return array
+}
+
+//not working, idk why it hates me
+
+console.log(countPositiveSumNegative([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15],[10, -65]]))
+console.log(countPositiveSumNegative([[3,-6,9,-12,15,0],[3,-18]]))
+console.log(countPositiveSumNegative([[],[]]))
