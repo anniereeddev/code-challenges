6b02e2d5f4818f9d81fd9fb338882f6b7759a7ee
anniereeddev <anniereed.dev@gmail.com>
2022-06-12 12:17:46 -0600

    Code challenge solution 6-11

--- CC 2022-6-10/app.js
+++ CC 2022-6-10/app.js
@@ -11,3 +11,28 @@ function positiveSum(arr) {
       arr.reduce
     }
   }
+
+//   second attempt
+
+// P: If there is nothing to sum, the sum is default to 0. Will be given an array of positive and negative numbers.  Will it ever have anything besides numbers in it? 
+
+//R: Return sum of positive numbers. 
+//E: Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//E: [3,20,5,-94] => 3 + 20 + 5 = 28
+//E: [] => 0
+
+//P: First need to take in a function that takes a parameter. 
+function positiveSum(arr) {
+    //Need to see whether numbers in string are positive or negative.
+    //If number is positive, add the numbers together.
+    //If number is negative, ignore number.
+    let newArr = arr.filter(e=>e>0) //filters through string for positive numbers
+    //If NAN, ignore.
+    //If array is empty, return 0.
+    //Return result.
+    return newArr.reduce((acc,c)=>acc+c,0) //adds numbers together. Remember to add 0 at end, it is the initial value.
+}
+
+console.log(positiveSum([1,-4,7,12]))
+console.log(positiveSum([3,20,5,-94]))
+console.log(positiveSum([]))
