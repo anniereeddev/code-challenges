dd2758d0dd6b83a8277fa53e20cb04ed32727bcb
anniereeddev <anniereed.dev@gmail.com>
2022-06-23 17:48:14 -0600

    Coding Challenge 6-24 first attempt

---
+++ CC 2022-6-24/app.js
@@ -0,0 +1,24 @@
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// first attempt
+
+// P(parameters):No parameters
+// R: Returns "Hello World!"
+// E: 33 => 'Hello World'
+// E: potato => 'Hello World'
+// E: [] => 'Hello World'
+
+// P:
+// Create function
+// Function takes in any input
+// returns Hello world
+
+function Greet(x){
+    let x = 'Hello World!'
+    return 'Hello World!'
+}
+
--- CC-2022-6-17/app.js
+++ CC-2022-6-17/app.js
@@ -20,3 +20,28 @@ function noSpace(x){
 // return remaining characters in string
 
 // Not sure how to run through the string and return only the characters. 
+
+//second attempt
+
+function noSpace(x){
+    let result = x.replace(/\s/g, '')
+    return result
+}
+
+//it worked! I've never heard of this syntax before.
+// \s means "one space"
+// \s+ means "one or more spaces"
+//  /g means replace all occurrences
+
+// community results
+
+function noSpace(x){
+    let result = x.replace(/\s/g, '')
+    return result
+  }
+
+//   This one uses replace, which I haven't heard of before 
+
+function noSpace(x){return x.split(' ').join('')}
+
+// this one splits the string at the spaces and joins it without spaces. 
