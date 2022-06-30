c67fd5a0018a9ef234442164d5a9bde7ea8290c8
anniereeddev <anniereed.dev@gmail.com>
2022-06-29 19:32:17 -0600

    Coding Challenge 6-28 first attempt

--- CC 2022-6-28/app.js
+++ CC 2022-6-28/app.js
@@ -5,8 +5,22 @@
 
 // Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
+// first attempt
+
+// p: function must take in a string
+// r: function gives out a number
 // Examples
 // "1234" --> 1234
 // "605"  --> 605
 // "1405" --> 1405
 // "-7" --> -7
+// p: 
+// create a function
+// use js to convert from string to number
+// return number
+
+const stringToNumber = function(str){
+    return null-'';
+  }
+
+// This is the opposite of what we saw in the "number to string" challenge so I thought this would work, but for some reason it doesn't seem to work. maybe I'm using the wrong subtract sign for javascript?
