30dcec30d691448210ce374c1d25407640a0d745
anniereeddev <anniereed.dev@gmail.com>
2022-07-18 19:15:56 -0600

    2022-06-28 coding challenge prompt

--- CC-2022-6-25/app.js
+++ CC-2022-6-25/app.js
@@ -12,7 +12,7 @@
 // E: 999 => '999'
 // E: 021 => '021'
 // P: Start with a function called numberToString. 
-// Function converts number to string using preset Javascript syntax. (toString?)
+// Function converts number to string using preset Javascript syntax.
 // Function returns string. 
 
 function numberToString(num){
@@ -20,3 +20,24 @@ function numberToString(num){
 }
 
 // It passed! I am BRILLIANT.
+
+// Other solutions:
+
+function numberToString(num) {
+    return ''+ num
+}
+
+// I like this solution. Very easy to read that the number is turning into a string. If someone is reading this code that didn't write it, they will know what is going on very easily. 
+
+function numberToString(num) {
+    return String(num)
+}
+
+// This is another very good one, readable code and another function that I didn't know about in Javascript.
+
+// additional more complicated solutions:
+
+const numberToString = num => '${num}'
+
+// this solution works too, it's another pretty simple way to convert the number to a string, I'm not super good at this syntax though.
+
