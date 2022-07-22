c28f06daebcba4a829fdc02da87ed7b60b2f6f99
anniereeddev <anniereed.dev@gmail.com>
2022-07-22 13:12:17 -0600

    Problem statement for git challenge 7-22

---
+++ CC 2022-7-22/app.js
@@ -0,0 +1,19 @@
+// Introduction
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task
+
+// Given a year, return the century it is in.
+
+// First Attempt:
+
+// P: Create a function that accepts any year. 
+// R: Century that year is in.
+// E: 1705 --> 18
+// E: 1900 --> 19
+// E: 1601 --> 17
+// E: 2000 --> 20
+// P: Write a function called century
+// Century accepts the year.
+// Function analyzes compares year against centuries and matches to correct century.
+// Function returns century.
