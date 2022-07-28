4aa702964656823f43e7743ba4bf285544e01c12
anniereeddev <anniereed.dev@gmail.com>
2022-07-28 13:46:35 -0600

    7-26 code challenge other solutions

--- CC 2022-7-26/app.js
+++ CC 2022-7-26/app.js
@@ -38,5 +38,21 @@ function greet(name) {
     return "Hello " + name + ", how are you today?";
 }
 
-// It worked! I don't think that should have been as hard lol
+// It worked! I don't think that should have been as hard lol. Let's look at other solutions. Here's one I thought should have worked. 
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
+
+//I feel like mine was exactly the same as this one...why did theirs work? argh
+
+const greet = name => `Hello, ${name} how are you doing today?`;
+
+// This is a nice shorter version
+
+function greet(name) {
+    return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`
+  }
+
+//   What the flippers is going on here??? are those...letters???
 
