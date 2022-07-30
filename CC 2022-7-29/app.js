ca3b67a2abaa8ef3da66999ad1eca75d30ecf9ba
anniereeddev <anniereed.dev@gmail.com>
2022-07-29 20:38:00 -0600

    coding challenge prompt and folder updates

---
+++ CC 2022-7-29/app.js
@@ -0,0 +1,21 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+
+// First attempt: 
+
+//P: Function takes in two numbers and applies an operation to them.
+// R: Returns the result of the two numbers after the operation has been applied.
+// E ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+// P: Create a function basicOp
+// Find a function that will accept an operation and the numbers.
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+}
