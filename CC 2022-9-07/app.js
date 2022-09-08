fe0bf87f09982ae0d29f32669d0544565bd3a4e9
anniereeddev <anniereed.dev@gmail.com>
2022-09-07 19:29:58 -0600

    Coding challenge 09-07 first attempt

---
+++ CC 2022-9-07/app.js
@@ -0,0 +1,27 @@
+// Convert number to reversed array of digits
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// P: Non-negative number.
+// R: Digits of number in an array in reverse order
+// E: 
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+// P: Make a function called digitize
+
+// function digitize(n) {
+//     // split into a string
+//     var makeArr = n.split("");
+//     // reverse array
+//     var reverseArr = splitString.reverse();
+//     // return 
+//     return reverseArr;
+// }
+
+function digitize(n) {
+    var makeArr = n.split("");
+    var reverseArr = splitString.reverse();
+    return reverseArr;
+}
+
+// fail :'( what did I do wrong
