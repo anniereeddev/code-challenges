c8f4a6721e3c9ab324b3b0b19e10bf458bd948b3
anniereeddev <anniereed.dev@gmail.com>
2022-09-11 19:36:51 -0600

    Coding challenge 09-11 Coding challenge attempt 1

---
+++ CC 2022-9-11/app.js
@@ -0,0 +1,19 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// P: Function takes two words with one space in between them.
+// R: Two capital letters with a dot separating them. 
+// E: 
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+// P: Start with a function called function abbrevName(name)
+// I have no idea how to do this... let's attempt something. 
+
+function apprevName(name){
+    let index = 1
+    return SVGComponentTransferFunctionElement.charAT(index); 
+}
