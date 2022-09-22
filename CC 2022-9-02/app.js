869f8eb8a5e9dd5afc40c394c5d2551ec1f63143
anniereeddev <anniereed.dev@gmail.com>
2022-09-22 16:17:49 -0600

    third attempt

--- CC 2022-9-02/app.js
+++ CC 2022-9-02/app.js
@@ -9,8 +9,18 @@
 // idk why this is called maps when it has nothing to do with maps
 
 // first attempt
-function maps(x){
-    return x*2;
-}
+// function maps(x){
+//     return x*2;
+// }
 
 // Didn't work because of flippin zero
+
+function maps(x){
+    if x=0{
+        return = 0;
+    }else{
+        return x*2;
+    }
+}
+
+// didn't work, I think my syntax is wrong
