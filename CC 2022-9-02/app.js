310f600324b3c37a608a3b5a4a97d281b81faa14
anniereeddev <anniereed.dev@gmail.com>
2023-09-02 16:04:45 -0600

    coding challenge 1st attempt

---
+++ CC 2022-9-02/app.js
@@ -0,0 +1,16 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// P: Input: array of integers.
+// R: Array with value doubled.
+// E:
+// 1,2,3,4,5 --> 2,4,6,8,10
+// P: Make a function called maps
+
+// idk why this is called maps when it has nothing to do with maps
+
+// first attempt
+function maps(x){
+    return x*2;
+}
+
+// Didn't work because of flippin zero
